42569d0c8b803cd490ba2eda46444920460d4894
Luke Knudsen <lukejknudsen@gmail.com>
2019-10-12 17:45:07 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import Konva from 'konva';
+import { render } from 'react-dom';
+import { Stage, Layer, Star, Text } from 'react-konva';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  handleDragStart = e => {
+    e.target.setAttrs({
+      shadowOffset: {
+        x: 15,
+        y: 15
+      },
+      scaleX: 1.1,
+      scaleY: 1.1
+    });
+  };
+  handleDragEnd = e => {
+    e.target.to({
+      duration: 0.5,
+      easing: Konva.Easings.ElasticEaseOut,
+      scaleX: 1,
+      scaleY: 1,
+      shadowOffsetX: 5,
+      shadowOffsetY: 5
+    });
+  };
+  render() {
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Try to drag a star" />
+          {[...Array(10)].map((_, i) => (
+            <Star
+              key={i}
+              x={Math.random() * window.innerWidth}
+              y={Math.random() * window.innerHeight}
+              numPoints={5}
+              innerRadius={20}
+              outerRadius={40}
+              fill="#89b717"
+              opacity={0.8}
+              draggable
+              rotation={Math.random() * 180}
+              shadowColor="black"
+              shadowBlur={10}
+              shadowOpacity={0.6}
+              onDragStart={this.handleDragStart}
+              onDragEnd={this.handleDragEnd}
+            />
+          ))}
+        </Layer>
+      </Stage>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<App />, document.getElementById('root'));
